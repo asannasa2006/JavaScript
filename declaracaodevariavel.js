c1187b759365a5ee3e6a54fff9e11e8b7c79b183
asannasa2006 <biaanna104@gmail.com>
2024-05-14 09:30:18 -0300

    feat: declarando var

---
+++ declaracaodevariavel.js
@@ -0,0 +1,14 @@
+// tipo var 
+// foi a primeira forma de declarar variáveis que surgiu no JavaScript
+// funciona basicamente em qualquer parte do código
+
+var altura = 5;
+var comprimento = 7;
+
+// calcular a área de um quadrado/ retangulo 
+
+area = altura * comprimento;
+//diremos que area é uma variável que guardará o resultado da variável altura multiplicado pela variável comprimento
+
+console.log(area);
+//exibir o resultado no console para checarmos qual será a saída
