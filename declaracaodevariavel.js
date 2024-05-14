e9c926103afa5573d9e213a7e995f1a6b24a3a70
asannasa2006 <biaanna104@gmail.com>
2024-05-14 09:46:36 -0300

    feat: declarando let

--- declaracaodevariavel.js
+++ declaracaodevariavel.js
@@ -9,6 +9,43 @@ var comprimento = 7;
 
 area = altura * comprimento;
 //diremos que area é uma variável que guardará o resultado da variável altura multiplicado pela variável comprimento
+//não precisamos declarar a variável area, isso acontece pois o node acaba fazendo essa função
+// o var tem uma função especial que permite usá-la antes de ser declarada
 
 console.log(area);
 //exibir o resultado no console para checarmos qual será a saída
+
+
+
+//tipo let 
+//mudar a variável de var para let
+//Como o var pode ser declarada, usada, substituída, sem muitas regras, isso pode causar um comportamento inesperado no código, ou seja, um bug.
+//
+
+let altura = 5;
+let comprimento = 7;
+
+area = altura * comprimento;
+//definir a area sem declarar o let?
+
+console.log(area);
+//Continua funcionando! Até o momento não da pra notar diferença entre let e var.
+
+// agora vamos exigir que o código precise verificar para fazer o calculo correspondente
+//uma comparação para descobrir se o valor da let é um retângulo ou triângulo
+
+let forma = 'retângulo'
+let altura = 5;
+let comprimento = 7;
+
+let area
+
+if (forma === 'retângulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
+
+
