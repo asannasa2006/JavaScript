1ecec650b41f8d012f0fca29438f3f8435d05b36
asannasa2006 <biaanna104@gmail.com>
2024-05-17 09:58:22 -0300

    Tipos: truthy  e falsy

---
+++ truthyfalsy.js
@@ -0,0 +1,29 @@
+console.log("......Truthy e Falsy também são tipos primitivos.......")
+//0 e 1
+
+console.log("---0 é = a False?-");
+console.log(0===false);
+
+console.log(" ---1 é = a True?-");
+console.log(1==true);
+
+console.log("---0 é = a True?-");
+console.log(0===true);
+
+console.log("---1 é = a False?-");
+console.log(1==false);
+
+console.log("---String vasia é = a false?-");
+console.log(""==false);
+
+let minhaVar;
+let testNull = null
+
+console.log(minhaVar) //tipo underfined
+console.log(testNull) // tipo null
+
+let numero=1;
+let texto="AS"
+
+console.log(typeof(minhaVar)) // tipo undefined 
+console.log(typeof(testNull)) // tipo object 
