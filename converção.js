cec3f47d1531bf6a7ec0f276df715cc564aedd55
asannasa2006 <biaanna104@gmail.com>
2024-06-11 09:22:45 -0300

    Novas atualizações

---
+++ converção.js
@@ -0,0 +1,21 @@
+// tipo de dado 
+console.log("boleanos");
+
+const numero = 456;
+const numerostring = "456";
+
+console.log( numero === numerostring);
+// diferentes
+console.log( numero == numerostring);
+//iguais 
+// o JS converte a string para numero e testa 
+// converção implicita 
+
+console.log(numero + numerostring)
+//ocorre uma concatenação ( converte o numero pra string)
+//456456
+// converção implicita 
+
+console.log( "converção explicita");
+
+let = numero(numerostring);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+// tipo de dado 
+console.log("boleanos")
+
+const numero = 456;
+const numerostring = "456"
+
+console.log( numero === numerostring)
